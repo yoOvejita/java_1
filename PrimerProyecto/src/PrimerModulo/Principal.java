7cb1de08a99bf7985cd41ca74f14bf8f856cfbe8
rusok <rusok@COMPU-DE-RUB>
2021-09-21 21:10:41 -0400

    Primer commit

---
+++ PrimerProyecto/src/PrimerModulo/Principal.java
@@ -0,0 +1,49 @@
+package PrimerModulo;
+
+
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Mira muchacho, tienes que 
+		int numerito = 5;
+		//Instrucciones:
+		//Primero debes 
+		//procurar
+		//no comentar mucho
+		
+		/*
+		  Hola
+		  este
+		  es
+		  un
+		  comentario
+		  */
+		System.out.println(2 + 2);
+		int nutella;
+		nutella = 2203040;
+		float flotante =5.6f;
+		double doble = 5.6;
+		String texto = "El numero telefonico es: ";//cadena
+		System.out.println(nutella);
+		System.out.println(flotante);
+		System.out.println(doble);
+		System.out.println(texto + nutella);
+		doble = 4;
+		double doble2 = 3.1415;
+		boolean valor = true;
+		valor = false;
+		
+		double resultado = doble / doble2;
+		float resultadoF = (float) (doble / doble2);
+		int resultadoE = (int) (doble / doble2);
+		
+		int num = 302;
+		int num2 = 400;
+		int  res = num / num2;
+		float resf = num / num2;
+		double resd = num / num2;
+	}
+
+}
